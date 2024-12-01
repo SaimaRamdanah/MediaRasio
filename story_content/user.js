c62fe92a4219dd6dbc681da4d99e2f035ba53287
SaimaRamdanah <107915696+SaimaRamdanah@users.noreply.github.com>
2024-12-01 23:12:40 +0800

    Media Rasio Upload

---
+++ story_content/user.js
@@ -0,0 +1,117 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5mY00nApzvL":
+        Script1();
+        break;
+      case "6U0ivEF7Iab":
+        Script2();
+        break;
+      case "6FenWoXM8Y1":
+        Script3();
+        break;
+      case "5oRmjuXbnYn":
+        Script4();
+        break;
+      case "6SajOk7e13z":
+        Script5();
+        break;
+      case "5mZmDLK4RyZ":
+        Script6();
+        break;
+      case "5jdjzeFjNkm":
+        Script7();
+        break;
+      case "69jR6Y7x8dI":
+        Script8();
+        break;
+      case "6bzyjqVE1PG":
+        Script9();
+        break;
+      case "5fw0sgE3jbS":
+        Script10();
+        break;
+      case "6HGNUmTeJ1T":
+        Script11();
+        break;
+      case "5yrvINropMt":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src = "1.mp3";
+audio.load();
+audio.play();
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.0;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.1;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.2;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.3;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.4;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.6;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.7;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.8;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.9;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 1.0;
+}
+
